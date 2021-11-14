9b3c560c13d61dc2b4d93aaf293c442cbb795ff3
Jiri Kantor <jiri.kantor@sap.com>
2021-11-14 14:59:08 +0100

    feat(xor-adapters-live): implement convert endpoint

--- frontend/packages/xor-adapters-live/src/rates/convert.ts
+++ frontend/packages/xor-adapters-live/src/rates/convert.ts
@@ -1,5 +1,24 @@
 import {CurrencyAmount} from "@xor/xor-domain";
+import {Axios} from "axios";
+import {parseCurrencyAmountList} from "@xor/xor-api-schema";
 
-export default async function convert(): Promise<CurrencyAmount[]> {
-    throw new Error("Not Implemented Yet");
+
+export default async function convert(
+    apiClient: Axios,
+    baseAmount: number,
+    baseCurrency: string,
+    targetCurrencies: string[]
+): Promise<CurrencyAmount[]> {
+    const currencyAmountList = await apiClient.get(
+        "/convert",
+        {
+            params: {
+                from: baseCurrency,
+                amount: baseAmount,
+                to: targetCurrencies
+            }
+        }
+    );
+
+    return parseCurrencyAmountList(currencyAmountList.data).amounts;
 }
