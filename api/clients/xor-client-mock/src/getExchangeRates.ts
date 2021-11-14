652c61a758d2d3557ced0850a23c6e594ba613df
Jiri Kantor <jiri.kantor@sap.com>
2021-11-14 14:58:01 +0100

    feat(xor-client-mock): add exchange rates by recalculating usd

--- api/clients/xor-client-mock/src/getExchangeRates.ts
+++ api/clients/xor-client-mock/src/getExchangeRates.ts
@@ -1,5 +1,29 @@
 import {CurrencyRates} from "@xor/xor-client-schema";
+import exchangeRatesUsd from "./exchangeRatesUsd.json";
 
-export default function getExchangeRates(): Promise<CurrencyRates> {
-    throw new Error("Not Implemented Yet");
+function recalculateRates(targetCurrency: string): CurrencyRates {
+    const targetValueRatePair = Object
+        .entries(exchangeRatesUsd.rates)
+        .find((o) => o && o[0] === targetCurrency);
+    if (!targetValueRatePair) {
+        throw new Error(`Currency ${targetCurrency} not found`);
+    }
+
+    const usdToTargetRate = targetValueRatePair[1];
+
+    return {
+        baseCurrency: targetCurrency,
+        validUntil: Math.floor(new Date().getTime() / 1000),
+        rates: Object.fromEntries(
+            Object
+                .entries(exchangeRatesUsd.rates)
+                .map(([code, rate]) => [code, rate / usdToTargetRate])
+        )
+
+    };
+
+}
+
+export default function getExchangeRates(baseCurrency: string): Promise<CurrencyRates> {
+    return Promise.resolve(recalculateRates(baseCurrency));
 }
