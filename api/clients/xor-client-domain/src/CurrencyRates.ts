dffe508630075c66164ddf9aecda87db57f4b853
Jiri Kantor <jiri.kantor@sap.com>
2021-11-17 12:06:37 +0100

    chore(xor-client-domain): note that validity is in miliseconds

--- api/clients/xor-client-domain/src/CurrencyRates.ts
+++ api/clients/xor-client-domain/src/CurrencyRates.ts
@@ -3,5 +3,5 @@ import {Rates} from "./Rates";
 export interface CurrencyRates {
     baseCurrency: string; // 3-letter code
     rates: Rates;
-    validUntil: number; // UNIX timestamp of rates validity
+    validUntil: number; // UNIX timestamp of rates validity IN MILLISECONDS
 }
