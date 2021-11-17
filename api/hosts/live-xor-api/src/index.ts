18f633a06418bc8f31bc05e4d38473d4ff7316ef
Jiri Kantor <jiri.kantor@sap.com>
2021-11-17 21:10:46 +0100

    feat(live-xor-api): add stats client

--- api/hosts/live-xor-api/src/index.ts
+++ api/hosts/live-xor-api/src/index.ts
@@ -1,10 +1,13 @@
 import {Runtime, startServer} from "@xor/xor-api";
 import {createClient} from "@xor/xor-client-openexchangerates";
+import {createClient as createStatsClient} from "@xor/xor-stats-client-fs";
 import {createCachedClient} from "@xor/xor-api-cache";
 
 const client = createCachedClient(createClient());
+const statsClient = createStatsClient();
 
 const runtime: Runtime = {
-    getExchangeRatesClient: () => client
+    getExchangeRatesClient: () => client,
+    getStatsClient: () => statsClient
 };
 startServer(runtime);
