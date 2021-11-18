3cdd0bf2e33c6d78f7ce0d2dd5da29a3e4f54055
Jiri Kantor <jiri.kantor@sap.com>
2021-11-18 20:12:51 +0100

    chore(xor-stats-client-fs): uptake new config structure

--- api/clients/xor-stats-client-fs/src/getStatsFilePath.ts
+++ api/clients/xor-stats-client-fs/src/getStatsFilePath.ts
@@ -2,7 +2,7 @@ import {isAbsolute, join} from "path";
 import {config} from "@xor/xor-config";
 
 export default function getStatsFilePath(): string{
-    return isAbsolute(config.api.statsDataFilePath)
-        ? config.api.statsDataFilePath
-        : join(process.cwd(), config.api.statsDataFilePath);
+    return isAbsolute(config.api.stats.dataFilePath)
+        ? config.api.stats.dataFilePath
+        : join(process.cwd(), config.api.stats.dataFilePath);
 }
