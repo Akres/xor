986194d78d6f4134370ea282468a4e4bd0a9fbcd
Jiri Kantor <jiri.kantor@sap.com>
2021-11-18 20:13:28 +0100

    feat(live-xor-api): uptake aws stats client and new config

--- api/hosts/live-xor-api/src/index.ts
+++ api/hosts/live-xor-api/src/index.ts
@@ -1,7 +1,27 @@
 import {Runtime, startServer} from "@xor/xor-api";
 import {createClient} from "@xor/xor-client-openexchangerates";
-import {createClient as createStatsClient} from "@xor/xor-stats-client-fs";
+import {createClient as createStatsClientFs} from "@xor/xor-stats-client-fs";
+import {createClient as createStatsClientAws} from "@xor/xor-stats-client-aws";
 import {createCachedClient} from "@xor/xor-api-cache";
+import {config, throwIfValueEmpty, throwIfValueNotInEnum} from "@xor/xor-config";
+
+
+// Verify config
+throwIfValueEmpty(config.api.stats.client, "api.statsClient");
+throwIfValueNotInEnum(config.api.stats.client, "api.statsClient", ["fs", "aws"]);
+
+function createStatsClient() {
+    /* eslint-disable indent */
+    switch (config.api.stats.client) {
+        case "fs":
+            return createStatsClientFs();
+        case "aws":
+            return createStatsClientAws();
+        default:
+            throw new Error("Unsupported stats client type. This should not be happening.");
+    }
+    /* eslint-enable indent */
+}
 
 const client = createCachedClient(createClient());
 const statsClient = createStatsClient();
